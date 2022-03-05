5bc08b74c8d2639a9e69f8f1f8d61e3bdcd83eac
Tiago <tiago.gpes@gmail.com>
2022-03-05 12:32:12 -0300

    Movie Card

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,20 @@
+import {ReactComponent as StarFull} from 'assets/img/star_full.svg';
+import {ReactComponent as StarHalf} from 'assets/img/star_half.svg';
+import {ReactComponent as StarEmpty} from 'assets/img/star_empty.svg';
+
+import './styles.css';
+
+function MovieStars() {
+    return (
+        <div className="dsmovie-stars-container">
+            <StarFull />
+            <StarFull />
+            <StarFull />
+            <StarHalf />
+            <StarEmpty />
+            <StarFull />
+        </div>
+    )
+}
+
+export default MovieStars;
